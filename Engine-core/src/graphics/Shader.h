ea4b8395fa766c5e390fce9bea5ee1df49aee53b
Khalid Ramadan <khalid1ramadan@gmail.com>
2018-02-01 06:36:07 +0200

    Add Shaders

--- Engine-core/src/graphics/Shader.h
+++ Engine-core/src/graphics/Shader.h
@@ -1,18 +1,39 @@
 #pragma once
 
-#include "../utils/fileutils.h"
-#include <GL\glew.h>
+#include <iostream>
+#include <vector>
+#include <GL/glew.h>
 
-namespace engine
-{
-	namespace graphics
-	{
+#include "../math/maths.h"
+#include "../utils/fileutils.h"
+
+namespace engine {
+	namespace graphics {
 
 		class Shader
 		{
 		private:
-			GLuint shader;
+			GLuint m_ShaderID;
+			const char* m_VertPath;
+			const char* m_FragPath;
 		public:
+			Shader(const char* vertPath, const char* fragPath);
+			~Shader();
+
+
+			void setUniform1f(const GLchar* name, float value);
+			void setUniform1i(const GLchar* name, int value);
+			void setUniform2f(const GLchar* name, const math::vec2& vector);
+			void setUniform3f(const GLchar* name, const math::vec3& vector);
+			void setUniform4f(const GLchar* name, const math::vec4& vector);
+			void setUniformMat4(const GLchar* name, const math::mat4& matrix);
+
+			void enable() const;
+			void disable() const;
+		private:
+			GLuint load();
+			GLint getUniformLocation(const GLchar* name);
 		};
+
 	}
 }
